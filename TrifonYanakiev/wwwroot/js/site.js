00c5c3422a250050a9b2d4b890090334bdc73e1b
Trifon <trifonyanakiev@mymail.nwtc.edu>
2019-03-01 18:11:36 -0600

    Change of layout

--- TrifonYanakiev/wwwroot/js/site.js
+++ TrifonYanakiev/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(document).ready(function () {
+    $('body').scrollspy({ target: '#navbar' })
+})
