e10c94882280e81e2b32564fed5589c43522317c
Trifon <trifonyanakiev@mymail.nwtc.edu>
2019-03-23 17:56:14 -0500

    Adding data

---
+++ TrifonYanakiev/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace TrifonYanakiev.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
